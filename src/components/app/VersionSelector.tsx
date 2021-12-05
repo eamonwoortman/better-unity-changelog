07f72171aa9d21b124ce5fdc4b1d0e51075404e4
Eamon Woortman <eamon.woortman@gmail.com>
2021-12-05 12:39:25 +0100

    Tad cleanup

--- src/components/app/VersionSelector.tsx
+++ src/components/app/VersionSelector.tsx
@@ -1,4 +1,3 @@
-
 import { Catalog, CatalogEntry } from '../../types/changelog.types';
 import { Fragment, useEffect, useState } from 'react'
 import { Listbox, Transition } from '@headlessui/react'
@@ -12,8 +11,6 @@ function classNames(...classes) {
     return classes.filter(Boolean).join(' ')
 }
 
-
-
 export function VersionSelector (props: IVersionSelectorProps) {
     const [selected, setSelected] = useState<CatalogEntry>();
     const [catalog, setCatalog] = useState(props.catalog);
@@ -22,71 +19,71 @@ export function VersionSelector (props: IVersionSelectorProps) {
             setCatalog(props.catalog);
             setSelected(props.catalog.changelogs[0]);
         }
-      }, [props.catalog]);
+        }, [props.catalog]);
 
-  return (
+    return (
     <Listbox value={selected} onChange={setSelected} disabled={!catalog}>
-      {({ open }) => (
+        {({ open }) => (
         <>
-          <Listbox.Label className="block text-sm font-medium text-gray-700">Unity Version</Listbox.Label>
-          <div className="mt-1 relative">
+            <Listbox.Label className="block text-sm font-medium text-gray-700">Unity Version</Listbox.Label>
+            <div className="mt-1 relative">
             <Listbox.Button className="relative w-full bg-white border border-gray-300 rounded-md shadow-sm pl-3 pr-10 py-2 text-left cursor-default focus:outline-none focus:ring-1 focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm">
-              <span className="flex items-center">
+                <span className="flex items-center">
                 <span className="ml-3 block truncate">{selected?.version}</span>
-              </span>
-              <span className="ml-3 absolute inset-y-0 right-0 flex items-center pr-2 pointer-events-none">
+                </span>
+                <span className="ml-3 absolute inset-y-0 right-0 flex items-center pr-2 pointer-events-none">
                 <SelectorIcon className="h-5 w-5 text-gray-400" aria-hidden="true" />
-              </span>
+                </span>
             </Listbox.Button>
 
             <Transition
-              show={open}
-              as={Fragment}
-              leave="transition ease-in duration-100"
-              leaveFrom="opacity-100"
-              leaveTo="opacity-0"
+                show={open}
+                as={Fragment}
+                leave="transition ease-in duration-100"
+                leaveFrom="opacity-100"
+                leaveTo="opacity-0"
             >
-              <Listbox.Options className="absolute z-10 mt-1 w-full bg-white shadow-lg max-h-56 rounded-md py-1 text-base ring-1 ring-black ring-opacity-5 overflow-auto focus:outline-none sm:text-sm">
+                <Listbox.Options className="absolute z-10 mt-1 w-full bg-white shadow-lg max-h-56 rounded-md py-1 text-base ring-1 ring-black ring-opacity-5 overflow-auto focus:outline-none sm:text-sm">
                 {catalog?.changelogs.map((changelog, index) => (
-                  <Listbox.Option
+                    <Listbox.Option
                     key={index}
                     className={({ active }) =>
-                      classNames(
+                        classNames(
                         active ? 'text-white bg-indigo-600' : 'text-gray-900',
                         'cursor-default select-none relative py-2 pl-3 pr-9'
-                      )
+                        )
                     }
                     value={changelog}
-                  >
+                    >
                     {({ selected, active }) => (
-                      <>
+                        <>
                         <div className="flex items-center">
-                          <span
+                            <span
                             className={classNames(selected ? 'font-semibold' : 'font-normal', 'ml-3 block truncate')}
-                          >
+                            >
                             {changelog.version}
-                          </span>
+                            </span>
                         </div>
 
                         {selected ? (
-                          <span
+                            <span
                             className={classNames(
-                              active ? 'text-white' : 'text-indigo-600',
-                              'absolute inset-y-0 right-0 flex items-center pr-4'
+                                active ? 'text-white' : 'text-indigo-600',
+                                'absolute inset-y-0 right-0 flex items-center pr-4'
                             )}
-                          >
+                            >
                             <CheckIcon className="h-5 w-5" aria-hidden="true" />
-                          </span>
+                            </span>
                         ) : null}
-                      </>
+                        </>
                     )}
-                  </Listbox.Option>
+                    </Listbox.Option>
                 ))}
-              </Listbox.Options>
+                </Listbox.Options>
             </Transition>
-          </div>
+            </div>
         </>
-      )}
+        )}
     </Listbox>
-  )
+    )
 }
