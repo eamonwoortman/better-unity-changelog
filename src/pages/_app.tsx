2915fcb49b382bedbe12d960d06a42d1b93a8596
Eamon Woortman <eamon.woortman@gmail.com>
2021-12-11 12:14:28 +0100

    Added default layout

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,14 +1,15 @@
-import '../styles/globals.css'
-
-import { Provider } from 'react-redux'
 import type { AppProps } from 'next/app'
-
+import { Provider } from 'react-redux'
 import store from '../app/store'
+import Layout from '../components/layout'
+import '../styles/globals.css'
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   return (
     <Provider store={store}>
-      <Component {...pageProps} />
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
     </Provider>
   )
 }
