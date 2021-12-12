7df2811f8eb35839dcc66a2704217aba796f5e06
Eamon Woortman <eamon.woortman@gmail.com>
2021-12-12 09:52:11 +0100

    Change layout style so main content grows

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -3,10 +3,10 @@ import { Header } from './header'
 
 export default function Layout({ children }) {
   return (
-    <>
+    <div className="flex flex-col min-h-screen ">
       <Header />
-      <main>{children}</main>
+      <main className="flex-grow">{children}</main>
       <Footer />
-    </>
+    </div>
   )
 }
