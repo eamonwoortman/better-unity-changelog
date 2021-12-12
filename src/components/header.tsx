59d77d99477cae25836a632da85e41e9d6d426a9
Eamon Woortman <eamon.woortman@gmail.com>
2021-12-12 10:02:37 +0100

    Fixed relative path to header icon

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -5,7 +5,7 @@ export const Header: FunctionComponent = () => <header>
     <section className="relative w-full px-8 text-gray-700 bg-black body-font">
         <div className="container flex flex-col flex-wrap items-center justify-between py-5 mx-auto md:flex-row max-w-7xl">
           <div className="relative z-10 flex items-center w-auto text-2xl font-extrabold leading-none text-black select-none">
-              <img src="assets/unity-logo.svg" className="h-[32px]" alt="logo" />
+              <img src="/assets/unity-logo.svg" className="h-[32px]" alt="logo" />
             </div>
           <div className="top-0 left-0 z-0 flex items-center justify-center w-full h-full py-5 -ml-0 space-x-5 text-base md:-ml-5 md:py-0 md:absolute">
             <ul className="flex">
