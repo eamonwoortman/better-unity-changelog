cb4f428dbada206b1aa474f52e0afacd13134516
Eamon Woortman <eamon.woortman@gmail.com>
2022-01-30 11:15:19 +0100

    Conditionally hidden entire logo div

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -7,7 +7,7 @@ export const Header: FunctionComponent = () => {
   const showThemeSwitch: boolean = true;
   return (<header className="sticky top-0 bg-black">
       <div className="flex w-full p-6 items-center max-w-8xl mx-auto">
-        <div className="cursor-pointer mr-2">
+        <div className="cursor-pointer mr-2 hidden md:inline-flex">
           <img src="/assets/unity-logo.svg" className="h-[32px] hidden lg:inline-flex" alt="logo" onClick={() => router.push('/')} />
           <img src="/assets/unity-icon.png" className="h-[42px] hidden md:inline-flex lg:hidden" alt="logo" onClick={() => router.push('/')} />
         </div>
