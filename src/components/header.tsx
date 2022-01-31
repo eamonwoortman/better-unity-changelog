0405cb883e7c1db2095ac796daef7cc8b61ea5b4
Eamon Woortman <eamon.woortman@gmail.com>
2022-01-31 19:03:10 +0100

    Added h-top to main layout and increased the z-order of the navigation bar

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -5,7 +5,7 @@ import { ThemeSwitch } from "./themeselector/ThemeSwitch";
 
 export const Header: FunctionComponent = () => {
   const showThemeSwitch: boolean = true;
-  return (<header className="sticky top-0 bg-black">
+  return (<header className="sticky top-0 z-50 bg-black">
       <div className="flex w-full p-6 items-center max-w-8xl mx-auto">
         <div className="cursor-pointer mr-2 hidden md:inline-flex">
           <img src="/assets/unity-logo.svg" className="h-[32px] hidden lg:inline-flex" alt="logo" onClick={() => router.push('/')} />
