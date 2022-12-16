ec3fa4f8812f1ae104daeceb149e32e984977484
Eamon Woortman <eamon.woortman@gmail.com>
2022-12-16 22:43:44 +0100

    Reverted appDir changes

--- next.config.js
+++ next.config.js
@@ -1,7 +1,4 @@
 module.exports = {
-    experimental: {
-        appDir: true,
-    },
     future: {
         webpack5: true, // by default, if you customize webpack config, they switch back to version 4. 
         // Looks like backward compatibility approach.
