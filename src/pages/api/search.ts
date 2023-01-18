4a1a038413dc7a8f91b869d78b724fac9396aeda
Eamon Woortman <eamon.woortman@gmail.com>
2023-01-18 21:08:20 +0100

    Returned direct result from searchVersionAc, without checking the query

--- src/pages/api/search.ts
+++ src/pages/api/search.ts
@@ -2,12 +2,9 @@ import type { NextApiHandler } from 'next';
 import { ChangelogDatabase } from '../../services/changelogdb';
 
 const searchHandler: NextApiHandler = async (req, res) => {
-    const searchQuery : string | string[] = req.query.version;
-    if (searchQuery) {
-      const result = await ChangelogDatabase.searchVersionAc(searchQuery);
-      return res.send(result);
-    }
-    res.send([]);
+    const searchQuery : string = req.query.version as string;
+    const result = await ChangelogDatabase.searchVersionAc(searchQuery);
+    return res.send(result);
 }
 
 export default searchHandler
