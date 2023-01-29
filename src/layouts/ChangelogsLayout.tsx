3f5cac3e260009289beca84fa256458d221c0895
Eamon Woortman <eamon.woortman@gmail.com>
2023-01-29 18:03:15 +0100

    Used custom context provider to fix hot reload issues

--- src/layouts/ChangelogsLayout.tsx
+++ src/layouts/ChangelogsLayout.tsx
@@ -1,31 +1,46 @@
-import { createContext, ReactElement, useEffect, useRef, useState } from "react";
+import React, { createContext, Dispatch, ReactElement, SetStateAction, useEffect, useState } from "react";
 import { Footer } from "../components/footer";
 import { Header } from "../components/header";
 import TableOfContents from "../components/sidebar/TableOfContents";
 import { ChangelogRoot } from "../features/changelogs/changelog.types";
 
-export const ChangelogsContext = createContext(null);
 
+type State = ChangelogRoot[];
+type ContextType = {changelogs: ChangelogRoot[]; setChangelogs: Dispatch<SetStateAction<ChangelogRoot[]>>} | undefined;
+
+export const ChangelogsContext = createContext<ContextType>(undefined);
+
+function useChangelogs() {
+    const context = React.useContext(ChangelogsContext)
+    if (context === undefined) {
+      throw new Error('useCount must be used within a CountProvider')
+    }
+    return context
+  }
+
+function ChangelogsProvider({children}: {children: React.ReactNode}) {
+    const [changelogs, setChangelogs] = useState<ChangelogRoot[]>([]);
+    const value = {changelogs, setChangelogs}
+    return (
+      <ChangelogsContext.Provider value={value}>
+        {children}
+      </ChangelogsContext.Provider>
+    )
+  }
 
 export default function ChangelogsLayout(page: ReactElement) {
-    const pageRef = useRef(page);
-    const [changelogs, setChangelogs] = useState<ChangelogRoot[]>([]);
 
     useEffect(() => {
         console.log('RENDER');
     }, [page])
 
-
     return (
-        <ChangelogsContext.Provider value={{
-            changelogs,
-            setChangelogs
-          }}>
+        <ChangelogsProvider>
             <main className="flex flex-col h-screen">
                 <Header />
 
                 <div className="flex flex-1 overflow-hidden">
-                    <div className="flex bg-gray-100 w-32 p-4">
+                    <div className="flex bg-gray-100 w-45 p-4">
                         <TableOfContents page={page} />
                     </div>
                     <div className="flex flex-1 flex-col">
@@ -36,5 +51,7 @@ export default function ChangelogsLayout(page: ReactElement) {
                 </div>
 
                 <Footer />
-            </main></ChangelogsContext.Provider>)
+            </main>
+        </ChangelogsProvider>
+    )
 }
