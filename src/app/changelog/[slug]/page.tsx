c125baa1bfe6308723664516214b7aa70d1a45d2
Eamon Woortman <eamon.woortman@gmail.com>
2023-02-09 17:33:14 +0100

    Changed page layout and added new changelog page

---
+++ src/app/changelog/[slug]/page.tsx
@@ -0,0 +1,79 @@
+
+import { ChangelogRoot } from '../../../components/changelog/changelog.types'
+import ChangelogContainer from '../../../components/changelog/ChangelogContainer'
+import ChangeLogDetailLayout from '../../../layouts/ChangelogDetailLayout'
+import { ChangelogDatabase } from '../../../services/changelogdb'
+import { Version } from '../../../utils/vparse'
+
+type ChangelogsPageProps = ChangelogRoot[];
+
+type ChangelogProps = {
+  params: { slug: string };
+  searchParams?: { [key: string]: string | string[] | undefined };
+}
+
+export default async function Page(props: ChangelogProps) {
+  const changelogs = await getServerSideProps(props)
+
+  return (<>
+      <ChangeLogDetailLayout changelogs={changelogs}>
+        {changelogs !== undefined && changelogs.map((changelog:ChangelogRoot, index:number) => (
+          <ChangelogContainer key={index} root={changelog} id={index} />
+          )
+        )}
+      </ChangeLogDetailLayout>
+    </>)
+  ;
+}
+const getChangelog = async (versionString: string) : Promise<ChangelogRoot> => {
+  const version:Version = Version.parseVersion(versionString)
+
+  if (version.isEmpty) {
+    console.log(`isempty: ${versionString}`)
+
+    return null
+  }
+
+  return await ChangelogDatabase.findVersion<ChangelogRoot>(version.text)
+}
+
+const getChangelogs = async (fromQuery: string, toQuery: string) : Promise<ChangelogRoot[]> => {
+  const from:Version = Version.parseVersion(fromQuery)
+  const to:Version = Version.parseVersion(toQuery)
+
+  if (from.isEmpty || to.isEmpty) {
+    console.log(`Could not parse 'from'(${fromQuery}) or 'to'(${toQuery}) queries`)
+
+    return []
+  }
+
+  const result = await ChangelogDatabase.findVersions(from, to)
+
+  return result as ChangelogRoot[]
+}
+
+export async function getServerSideProps(props: ChangelogProps): Promise<ChangelogsPageProps> {
+  const { params, searchParams } = props
+
+  if (!params) {
+    return []
+  }
+  console.log('TEST', params, searchParams)
+  const changelogs = []
+
+  if (params.slug != undefined) {
+    const matchingChangelog = await getChangelog(params.slug)
+
+    if (matchingChangelog) {
+      changelogs.push(matchingChangelog)
+    }
+  } else if (searchParams != undefined) {
+
+    /*
+     * Var [ from, to ] = searchParams;
+     * changelogs = await getChangelogs(from, to)
+     */
+  }
+
+  return changelogs
+}
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,23 +1,30 @@
-import { ThemeProvider } from "../providers/themeprovider";
-import StoreProvider from "../providers/storeprovider";
+import { Footer } from '../components/footer'
+import { Header } from '../components/header'
+import Providers from '../providers/providers'
+import '../styles/globals.css'
 
 export default function RootLayout({
-    // Layouts must accept a children prop.
-    // This will be populated with nested layouts or pages
-    children,
+
+  /*
+   * Layouts must accept a children prop.
+   * This will be populated with nested layouts or pages
+   */
+  children
 }: {
     children: React.ReactNode;
 }) {
 
-    return (
-        <html lang="en">
-            <body>
-                <StoreProvider>
-                    <ThemeProvider>
-                        {children}
-                    </ThemeProvider>
-                </StoreProvider>
-            </body>
-        </html>
-    );
+  return (
+    <html lang="en">
+      <body>
+        <Providers>
+          <div className="flex flex-col min-h-screen">
+            <Header />
+            <main className="flex-grow dark:bg-gray-800 dark:text-gray-200">{children}</main>
+            <Footer />
+          </div>
+        </Providers>
+      </body>
+    </html>
+  )
 }
