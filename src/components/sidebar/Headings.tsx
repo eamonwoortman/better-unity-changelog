f9d0e248844f2317a460747229bb8070c0d953b8
Eamon Woortman <eamon.woortman@gmail.com>
2023-02-16 22:33:12 +0100

    Added styling to sidebar nav links

--- src/components/sidebar/Headings.tsx
+++ src/components/sidebar/Headings.tsx
@@ -1,9 +1,9 @@
-export function Headings({ headings }) {
-  return (<ul>
+export function Headings({ headings, activeId }) {
+  return (<ul className="nav nav-pills menu-sidebar">
     {headings.map((heading) => (
       <li key={heading.id} className={`ml-${heading.depth}`}>
-        <a href={`#${heading.id}`}>{heading.title}</a>
-        {heading.items.length > 0 && <Headings headings={heading.items}/>}
+        <a className={`nav-link ${activeId === heading.id ? 'active' : ''}`} href={`#${heading.id}`}>{heading.title}</a>
+        {heading.items.length > 0 && <Headings headings={heading.items} activeId={activeId}/>}
       </li>
     ))}
   </ul>);
